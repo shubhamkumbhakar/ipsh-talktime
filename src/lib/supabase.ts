a85702875dffb275e46a9add6bc5c0a74d7b1abe
shubhamkumbhakar <kumbhakarshubham27@gamil.com>
2026-04-22 21:31:57 +0530

    env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL ?? 'https://gowlsoqlupqzqasxzued.supabase.co'
 const supabaseAnonKey =
-  import.meta.env.VITE_SUPABASE_ANON_KEY;
+  import.meta.env.VITE_SUPABASE_ANON_KEY ?? 'sb_publishable_emmI4yVDcErqLSC2fx0tSA_k5rB8nj0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
